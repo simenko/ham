6c80df090b3601c11c7b9165c28c0d69724dff6d
Ilya Simenko <ilya@thinkpad.(none)>
2011-05-18 20:49:22 +0300

    first draft

--- index.js
+++
@@ -1,5 +0,0 @@
-exports.Message = require('./lib/message');
-exports.Hub = require('./lib/hub');
-exports.Storage = require('./lib/storage');
-exports.Processor = require('./lib/processor');
-exports.Interface = require('./lib/interface');
