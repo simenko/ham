5bd4335c5c0466dba24571a2303c0be060cf5134
Ilya Simenko <ilya42@gmail.com>
2011-05-21 20:19:43 +0300

    workers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.Hub = require('./lib/hub');
 exports.Storage = require('./lib/storage');
-exports.Processor = require('./lib/processor');
+exports.Worker = require('./lib/worker');
 exports.Interface = require('./lib/interface');
